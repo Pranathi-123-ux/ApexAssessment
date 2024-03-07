ca4e31436ffea499762453f31f87765e51791e64
Pranathi-123-ux <78425507+Pranathi-123-ux@users.noreply.github.com>
2024-03-07 18:06:56 +0530

    Create secondquestion.java
    Created 2 recordTypes Named NewDeal And Servicing And also queried for newdeal records from the existed database where close data was 6 months ago and to those records now we cretaed new recordtype as servicing.

---
+++ secondquestion.java
@@ -0,0 +1,29 @@
+Question2:
+code:
+public class secondquestion implements schedulable{
+    public void execute(SchedulableContext sc)
+    {
+        Date dt=system.today().addMonths(-6);
+        List<Opportunity>oldrecords=[select Id,Name,StageName,RecordType.Name from Opportunity where StageName='Closed Won' and RecordType.name='New Deal' and CloseDate=:dt];
+        List<opportunity>newrecordsList=new List<Opportunity>();
+        Id devRecordTypeId = Schema.SObjectType.Opportunity.getRecordTypeInfosByName().get('servicing').getRecordTypeId();
+        for(Opportunity opp:oldrecords)
+        {       
+            Opportunity newrec=new Opportunity();
+            newrec.Name=opp.Name;
+            newrec.StageName='Prospecting';
+            newrec.RecordTypeId=devRecordTypeId;
+            newrec.CloseDate=System.Today().addDays(30);
+            newrecordsList.add(newrec);
+        }
+        if(newrecordsList.size()>0)
+        {
+            insert newrecordsList;
+        }
+        
+    }
+
+}
+Anonymous window:
+  String cronExp = '0 0 0 * * ?';
+System.schedule('inserting the values', cronExp, new secondquestion());
